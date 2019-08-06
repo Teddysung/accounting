c55f0d41f3b6fc1a5d4afb4f05ac302697880177
Teddy Sung <teddysung168@gmail.com>
2019-08-06 11:57:17 +0800

    you can use this version to add things into the Excel file you made from the first version (the second version can't make an Excel file)

---
+++ products2.py
@@ -0,0 +1,22 @@
+product = []
+with open('product.csv', 'r') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price, rate = line.strip().split(',')
+		product.append([name, price, rate])
+print(product)
+while True:
+	name = input('請輸入商品名稱: ')
+	if name == 'q':
+		break
+	price = input('請輸入價格: ')
+	rate = input('請給分: ')
+	product.append([name, price, rate])
+print(product)
+for p in product:
+	print(p[0], '的價格是', p[1])
+with open('product.csv', 'w') as f:
+	f.write('商品,價格,評分\n')
+	for p in product:
+		f.write(p[0] + ',' + p[1] + ',' + p[2] + '\n') 
